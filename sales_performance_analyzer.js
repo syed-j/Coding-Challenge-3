4c620dee4f6a601fcada1a807c2650ee12657863
Syed Jafri <ijtabajafripp@gmail.com>
2024-09-08 18:10:45 -0400

    fixed issues in step 3

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -30,8 +30,8 @@ function determinePerformanceRating(avgsales){
         return "Invalid"
     }
 }
+
 // Step #3 
-return [topPerformer,bottomPerformer]
 
 
 function findTopAndBottomPerformers_withReduce(persons){
